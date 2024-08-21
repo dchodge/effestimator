70247edb0fee0c28938070c5d92571f8da9fe7ea
David Hodgson <david_hodgson1991@hotmail.co.uk>
2024-08-21 14:35:29 +0100

    Added new parameterisation for the stan fit

--- R/model.R
+++ R/model.R
@@ -97,3 +97,102 @@ fitted_functions <- function(data_vac, name, lower_bounds) {
     ggsave(here::here("outputs", name, "summary_fig.png"), height = 10, width = 10)
 
 }
+
+
+fitted_functions_none <- function(data_vac, name) {
+
+    # define the data
+    data_list <- list(
+        N_pla = nrow(data_vac$placebo),
+        tp_pla = data_vac$placebo$tp,
+        t_pla = data_vac$placebo$t,
+        persons_pla =  data_vac$placebo$person_time,
+        n_pla = data_vac$placebo$n,
+
+        N_vac = nrow(data_vac$vaccine),
+        tp_vac = data_vac$vaccine$tp,
+        t_vac = data_vac$vaccine$t,
+        persons_vac = data_vac$vaccine$person_time,
+        n_vac = data_vac$vaccine$n
+    )
+
+    T <- max(data_vac$placebo$t)
+    mod <- cmdstan_model(here::here("src", "eff_est_none.stan"), compile = TRUE)
+
+    fit_stan_exp <- mod$sample(
+        data = c(model_id = 1, data_list), 
+     #   seed = 123, 
+        chains = 4, 
+        parallel_chains = 4,
+        refresh = 500 # print update every 500 iters
+    )
+    fit_stan_er2 <- mod$sample(
+      data = c(model_id = 2, data_list), 
+    #  seed = 123, 
+      chains = 4, 
+      parallel_chains = 4,
+      refresh = 500 # print update every 500 iters
+    )
+    fit_stan_er3 <- mod$sample(
+      data = c(model_id = 3, data_list), 
+     # seed = 123, 
+      chains = 4, 
+      parallel_chains = 4,
+      refresh = 500 # print update every 500 iters
+    )
+
+    if (!dir.exists(here::here("outputs", name))) {
+        # create the folder if it doesn't exist
+        dir.create(here::here("outputs", name))
+    }
+    
+    fit_stan_exp$save_object(file = here::here("outputs", name, "fits_exp.rda"))
+    fit_stan_er2$save_object(file = here::here("outputs", name, "fits_er2.rda"))
+    fit_stan_er3$save_object(file = here::here("outputs", name, "fits_er3.rda"))
+
+    fit_stan_exp %>% as_draws_df %>% spread_draws(wane_a, wane_b) %>% 
+      rename(wane_a_exp = wane_a, wane_b_exp = wane_b) -> posteriors_wane_exp
+    fit_stan_er2 %>% as_draws_df %>% spread_draws(wane_a, wane_b) %>% 
+      rename(wane_a_er2 = wane_a, wane_b_er2 = wane_b) -> posteriors_wane_er2
+    fit_stan_er3 %>% as_draws_df %>% spread_draws(wane_a, wane_b) %>% 
+      rename(wane_a_er3 = wane_a, wane_b_er3 = wane_b) -> posteriors_wane_er3
+    
+    posteriors_wane_exp %>% 
+      left_join(posteriors_wane_er2, by = c(".chain", ".iteration", ".draw")) %>% 
+      left_join(posteriors_wane_er3, by = c(".chain", ".iteration", ".draw")) -> posteriors_wane
+
+    save(posteriors_wane, file = here::here("outputs", name, paste0(name, "_post_wane.RData") ))
+
+    df_data_pla <- data.frame(
+        t = data_list$t_pla, 
+        inci = (data_list$n_pla) / (data_list$persons_pla)
+    )
+
+    df_data_vac <- data.frame(
+        t = data_list$t_vac, 
+        inci = (data_list$n_vac) / (data_list$persons_vac)
+    )
+
+    # Technically this fit (p1) is different for each waning fn. Just show exp fit for now
+    p1 <- fit_stan_exp %>% as_draws_df %>% spread_draws(incidence_pla[t], time_p[t]) %>%
+        ggplot() + 
+            stat_lineribbon(aes(time_p, incidence_pla), .width = 0.95, fill = "blue", alpha = 0.5) + 
+            geom_point(data = df_data_pla, aes(t, inci), shape = 21, fill = "red") +
+            theme_bw() + labs(x = "Time post-vaccination (days)", y = "Incidence (placebo group)")
+
+    p2 <- fit_stan_exp %>% as_draws_df %>% spread_draws(waning[t]) %>% 
+        ggplot() + 
+            stat_lineribbon(aes(t, waning), .width = 0.95, fill = "red", alpha = 0.5) + theme_bw() +
+            labs(x = "Time post-vaccination (days)", y = "Estimated efficacy")
+    p3 <- fit_stan_er2 %>% as_draws_df %>% spread_draws(waning[t]) %>% 
+        ggplot() + 
+            stat_lineribbon(aes(t, waning), .width = 0.95, fill = "red", alpha = 0.5) + theme_bw() +
+            labs(x = "Time post-vaccination (days)", y = "Estimated efficacy")
+    p4 <- fit_stan_er3 %>% as_draws_df %>% spread_draws(waning[t]) %>% 
+        ggplot() + 
+            stat_lineribbon(aes(t, waning), .width = 0.95, fill = "red", alpha = 0.5) + theme_bw() +
+            labs(x = "Time post-vaccination (days)", y = "Estimated efficacy")
+    (p1) / (p2 + p3 + p4)
+    ggsave(here::here("outputs", name, "summary_fig.png"), height = 10, width = 10)
+
+}
---
+++ src/eff_est_none.stan
@@ -0,0 +1,110 @@
+/// https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6120589/pdf/nihms1502750.pdf
+functions{
+    // functions assocated with waning
+    vector exp_wane(real wane_a, real wane_b, vector t_vac) {
+        return wane_a * exp(- (wane_b) * t_vac);
+    }
+    vector erlangk_wane(real wane_a, real wane_b, vector t_vac, int k) {
+        int N = size(t_vac);
+        vector[N] gamma_vec;
+        for (i in 1:N) {
+            gamma_vec[i] = gamma_cdf(t_vac[i]| k, wane_b);
+        }
+        return wane_a * (1 - gamma_vec);
+    }
+}
+data {
+    int model_id;
+  
+    int N_pla;
+    array[N_pla] int tp_pla;
+    vector[N_pla] t_pla; // number of time points in a vector (for gaussian process)
+    vector[N_pla] persons_pla;
+    array[N_pla] int n_pla;
+
+    int N_vac;
+    array[N_pla] int tp_vac;
+    vector[N_vac] t_vac;
+    vector[N_vac] persons_vac; // number of time points in a vector (for gaussian process)
+    array[N_vac] int n_vac;
+    
+} 
+transformed data {
+    real delta = 1e-10; // just a thing to stabilise gp
+}
+parameters{
+    real<lower = 0, upper = 1> wane_a; //??wane a rate for exponential waning
+    real<lower = 0, upper = 0.1> wane_b; //??wane b rate for exponential waning
+
+    // add in gaussian processes to model the incidence over time
+    real<lower = 0> alpha; // step size
+    real<lower = 0> rho; // length
+    vector[N_pla] eta; // non-centralisation parameters
+
+}
+transformed parameters {
+    vector[N_pla] inci_gp;
+    matrix[N_pla, N_pla] K;
+    matrix[N_pla, N_pla] L_K;
+
+    // This is the Gaussian process stuff, K, L_K are covariance matrices
+    K = gp_exp_quad_cov(tp_pla, alpha, rho) + diag_matrix(rep_vector(delta, N_pla));
+    L_K = cholesky_decompose(K);
+    inci_gp = L_K * eta;
+}
+model {
+    // exponential of inci_gp to ensure positivity 
+    n_pla ~ poisson(persons_pla .* exp(inci_gp));
+    if (model_id == 1) {
+      n_vac ~ poisson(persons_vac .* exp(inci_gp) .* (1 - exp_wane(wane_a, wane_b, t_vac)));
+    } else if (model_id == 2) {
+      n_vac ~ poisson(persons_vac .* exp(inci_gp) .* (1 - erlangk_wane(wane_a, wane_b, t_vac, 2)));
+    } else if (model_id == 3) {
+      n_vac ~ poisson(persons_vac .* exp(inci_gp) .* (1 - erlangk_wane(wane_a, wane_b, t_vac, 3)));
+    }
+
+    // GP priors 
+    rho ~ inv_gamma(5, 5);
+    alpha ~ normal(0, 1);
+    eta ~ std_normal(); 
+
+    // waning priors
+    wane_a ~ uniform(0, 1);
+    wane_b ~ uniform(0, 0.2);
+}
+generated quantities {
+    // this section just gives useful outputs
+    vector[N_pla] incidence_pla;
+    vector[N_pla] incidence_vac;
+    
+    vector[730] waning;
+
+    vector[730] time_points = linspaced_vector(730, 0, 730 - 1);
+    // Waning efficacy over 730 days
+    if (model_id == 1) {
+      waning = exp_wane(wane_a, wane_b, time_points);
+    } else if (model_id == 2) {
+      waning = erlangk_wane(wane_a, wane_b, time_points, 2);
+    } else if (model_id == 3) {
+      waning = erlangk_wane(wane_a, wane_b, time_points, 3);
+    }
+
+    // Waning efficacy at time points in data
+    vector[N_pla] waning_inc;
+
+    vector[N_pla] time_p;
+    if (model_id == 1) {
+      waning_inc = exp_wane(wane_a, wane_b, t_vac);
+    } else if (model_id == 2) {
+      waning_inc = erlangk_wane(wane_a, wane_b, t_vac, 2);
+    } else if (model_id == 3) {
+      waning_inc = erlangk_wane(wane_a, wane_b, t_vac, 3);
+    }
+
+    // Estimated incidence at time points in data, for comparison with data
+    for(i in 1:N_pla) {
+        time_p[i] = t_pla[i];
+        incidence_pla[i] = exp(inci_gp[i]);
+        incidence_vac[i] = exp(inci_gp[i]) * (1 - waning_inc[i]);
+    }
+}
